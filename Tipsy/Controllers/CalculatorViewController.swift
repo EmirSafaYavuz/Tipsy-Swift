2bd3fa19e461d91fe23850eeec095ac00336b29b
emirsafayavuz <emirsafayavuz@gmail.com>
2024-06-29 18:48:17 +0300

    Tip and Split numbers outlets and actions.

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
+    
+    var tipPercentage: Float = 0.1
+    var splitNumber: Int = 2
+    
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
@@ -17,15 +20,22 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var splitNumberLabel: UILabel!
 
     @IBAction func tipChanged(_ sender: UIButton) {
-        
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        sender.isSelected = true
+        tipPercentage = Float(sender.currentTitle!.dropLast())! / 100
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
-        
+        sender.minimumValue = 1
+        sender.maximumValue = 12
+        splitNumber = Int(sender.value)
+        splitNumberLabel.text = String(splitNumber)
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
-        
+        print(tipPercentage)
     }
 }
 
---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Emir Safa Yavuz on 29.06.2024.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+
+}
